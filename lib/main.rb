8831f5d4b64a29ee910ce55fb64bf3f66ebda870
Luke Zhou <sharlock@gmail.com>
2022-11-29 21:23:38 +1100

    finish

--- lib/main.rb
+++ lib/main.rb
@@ -3,25 +3,29 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'calc'
 
-puts 'Let us start:'
-puts 'Please input deposit amount:'
-input = gets.strip
-deposit = input.to_f
-puts 'Please input interest rate:'
-input = gets.strip
-rate = input.to_f
-puts 'Please input investment term:'
-input = gets.strip
-term = Calc::Term.parse(input)
-puts 'Please input interest paid option (Monthly, Quarterly, Annually, Maturity):'
-input = gets.strip
-option = Calc::Option.parse(input)
+begin
+  puts 'Let us start:'
+  puts 'Please input deposit amount:'
+  input = gets.strip
+  deposit = input.to_f
+  puts 'Please input interest rate:'
+  input = gets.strip
+  rate = input.to_f
+  puts 'Please input investment term:'
+  input = gets.strip
+  term = Calc::Term.parse(input)
+  puts 'Please input interest paid option (Monthly, Quarterly, Annually, Maturity):'
+  input = gets.strip
+  option = Calc::Option.parse(input)
 
-calculator = Calc::Calculator.new.tap do |c|
-  c.deposit = deposit
-  c.rate = rate
-  c.term = term
-  c.option = option
+  calculator = Calc::Calculator.new.tap do |c|
+    c.deposit = deposit
+    c.rate = rate
+    c.term = term
+    c.option = option
+  end
+
+  puts "Balance: #{calculator.balance}"
+rescue StandardError
+  puts 'Invalid Input'
 end
-
-puts "Balance: #{calculator.balance}"
